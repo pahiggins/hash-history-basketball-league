eff13eb0fc6bb9e27e8dd3e7ff5ac6ad775c1e09
Peter Higgins <pah@cenabo.com>
2018-05-10 16:22:09 +0100

    Add Navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import Home from './Home';
+import Players from './Players';
+import Teams from './Teams';
+import Navbar from './Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        React Router Course
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+
+          <Route exact path='/' component={Home} />
+          <Route path='/players' component={Players} />
+          <Route path='/teams' component={Teams} />
+        </div>
+      </Router>
     );
   }
 }
