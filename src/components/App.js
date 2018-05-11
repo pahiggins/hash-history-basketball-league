c663f483ec89121cb1d15f1648dd4c1a915f2eb6
Peter Higgins <pah@cenabo.com>
2018-05-11 15:07:52 +0100

    Fetching a team's articles

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ import Players from './Players';
 import Teams from './Teams';
 import Navbar from './Navbar';
 import TeamPage from './TeamPage';
+import Articles from './Articles';
 
 class App extends Component {
   render() {
@@ -22,6 +23,7 @@ class App extends Component {
             <Route path="/players" component={Players} />
             <Route path="/teams" component={Teams} />
             <Route exact path="/:teamId" component={TeamPage} />
+            <Route path="/:teamId/articles" component={Articles} />
             <Route render={() => <h1 className="text-center">Four oh Four</h1>} />
           </Switch>
         </div>
